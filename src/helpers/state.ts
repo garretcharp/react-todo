87250d98cc42f9705c41f5df45a5a89265a0bdd7
garretcharp <garretcharp@gmail.com>
2020-07-19 14:38:22 +0000

    fix: Localstorage cors error on react
    - React doesnt seem to like looping over localStorage by .key(index)

--- src/helpers/state.ts
+++ src/helpers/state.ts
@@ -1,9 +1,10 @@
 export const store = {
   each: (cb: Function) => {
-    for (let index = 0; index < localStorage.length; index++) {
-      const element = localStorage.key(index);
-      cb(element ? JSON.parse(element) : null)
-    }
+    Object.keys(localStorage).forEach(key => {
+      const element = localStorage[key]
+
+      cb(JSON.parse(element), key)
+    })
   },
   set: (key: string, value: any) => {
     localStorage.setItem(key, JSON.stringify(value))
